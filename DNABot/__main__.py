64a0cf774c9648f164e51013bb0cdd760ae51856
H Fisher <hugo.fisher@gmail.com>
2024-05-15 22:44:43 +1000

    Added logging config

--- DNABot/__main__.py
+++ DNABot/__main__.py
@@ -5,16 +5,42 @@
     Hugh/Hugo Fisher AKA laranzu
     Canberra, Australia
     hugo.fisher@gmail.com
+
+    CLI args
+        -debug          Logging level
+        -info           Logging level
 """
 
 import sys
+import logging as log
 
 from . import config
 
+def initLogging(args):
+    """My preferred logging setup"""
+    logLevel = log.WARNING
+    if "-debug" in args:
+        logLevel = log.DEBUG
+    elif "-info" in args:
+        logLevel = log.INFO;
+    params = {
+        'format':   "%(levelname)s %(message)s",
+        'datefmt':  "%H:%M:%S",
+        'level':    logLevel,
+    }
+    if "-fg" not in args:
+        params["filename"] = "./dnabot.log"
+    log.basicConfig(**params)
+
+
 def main(args):
     """Main control for DNABot"""
-    print("Hello world")
+    initLogging(args)
     config.load()
+    log.debug("debug?")
+    log.info("info?")
+    log.warning("warn?")
+    print("Hello world")
 
 if __name__ == "__main__":
     main(sys.argv)
