bdc0a7a33b89dc89e18115555efdb0f8f488e41e
H Fisher <hugo.fisher@gmail.com>
2024-05-16 15:50:25 +1000

    Start utility prog to run lots of bots for me

---
+++ spawnBots.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# Program to run lots of DNABots, each in own temp dir
+
+# Usage: ./spawnBots.py N
+
+import math, os, pathlib, shutil, sys, tempfile, time
+from os import path
+
+N = 8
+
+UNDO = 5
+
+# How many?
+if len(sys.argv) > 1:
+    N = int(sys.argv[1])
+if N > 16:
+    print("{} is a lot of bots".format(N))
+    print("You have {} seconds to Ctrl-C".format(UNDO))
+    time.sleep(UNDO)
+print("Starting {} bots...".format(N))
+
+# Dir setup is /tmp/botNNN/
+TMP = tempfile.gettempdir()
+
+# How many digits per bot name?
+d = int(math.ceil(math.log10(N + 1)))
+
+# Format for name, zero padded so all same length
+fmt = "Bot{{:0{:d}d}}".format(d)
+
+# Go!
+for i in range(1, N + 1):
+    fullPath = path.join(TMP, fmt.format(i))
+    # Delete existing dir, create new
+    if path.exists(fullPath):
+        shutil.rmtree(fullPath)
+    shutil.copytree("./Proto", fullPath)
+    #
+    print("Bot #{} in {}".format(i, fullPath))
+
+print("OK.")
