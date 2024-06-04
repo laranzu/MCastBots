8c6f6b41660f2c8ec00f6d3a52277c2324545ac5
H Fisher <hugo.fisher@gmail.com>
2024-06-04 20:05:07 +1000

    Channel has ipaddress object, not string

--- DNABot/mcast.py
+++ DNABot/mcast.py
@@ -22,38 +22,36 @@ class BasicChannel(object):
 
     def __init__(self, IPaddress, portNumber, senderID):
         """Create and connect new socket for group address"""
-        self.address = IPaddress
+        self.groupAddr = ipaddress.ip_address(IPaddress)
         self.destPort = portNumber
         self.sender = senderID
         self.seqNo = 1
         self.createSockets()
-        log.info("Connected to group channel {}:{} as {}".format(self.address, self.destPort, self.sender))
+        log.info("Connected to group channel {}:{} as {}".format(self.groupAddr, self.destPort, self.sender))
 
     def createSockets(self):
         """Input and output sockets for group channel"""
         # For listening
-        log.debug("Create input socket for {}:{}".format(self.address, self.destPort))
+        log.debug("Create input socket for {}:{}".format(self.groupAddr, self.destPort))
         self.input = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.input.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.input.bind((self.address, self.destPort))
+        self.input.bind((self.groupAddr.compressed, self.destPort))
         self.input.settimeout(1.0)
         # Although designed for multicast, can use localhost for testing
-        if ipaddress.ip_address(self.address).is_multicast:
-            log.debug("Add membership for {}".format(self.address))
-            binAddr = socket.inet_aton(self.address)
-            ip_mreqn = struct.pack('!4sIH', binAddr, socket.INADDR_ANY, 0)
+        if self.groupAddr.is_multicast:
+            log.debug("Add membership for {}".format(self.groupAddr))
+            ip_mreqn = struct.pack('!4sIH', self.groupAddr.packed, socket.INADDR_ANY, 0)
             self.input.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, ip_mreqn)
         # For sending
-        log.debug("Create output socket for {}:{}".format(self.address, self.destPort))
+        log.debug("Create output socket for {}:{}".format(self.groupAddr, self.destPort))
         self.output = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.output.connect((self.address, self.destPort))
+        self.output.connect((self.groupAddr.compressed, self.destPort))
         log.debug("BasicChannel sockets created")
 
     def close(self):
         """Close channel"""
-        if ipaddress.ip_address(self.address).is_multicast:
-            binAddr = socket.inet_aton(self.address)
-            ip_mreqn = struct.pack('!4sIH', binAddr, socket.INADDR_ANY, 0)
+        if self.groupAddr.is_multicast:
+            ip_mreqn = struct.pack('!4sIH', self.groupAddr.packed, socket.INADDR_ANY, 0)
             self.input.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, ip_mreqn)
         self.input.close()
         self.output.close()
@@ -64,7 +62,7 @@ class BasicChannel(object):
     def send(self, message):
         """Prefix with sender and sequence number, send"""
         msg = "{} {} ".format(self.sender, self.seqNo) + message
-        #self.output.sendto(msg.encode('UTF-8'), (self.address, self.destPort))
+        #self.output.sendto(msg.encode('UTF-8'), (self.groupAddr, self.destPort))
         self.output.send(msg.encode('UTF-8'))
         self.seqNo += 1
 
